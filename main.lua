720dccd1a0147928ce24f7272ca880b1f89e950c
Erquint <gness.na@gmail.com>
2015-11-23 07:57:33 +0300

    Dumb testing feature. Failed attempt at fixing Game Over state minor bug involving variables globalization(left hanging midway there; needs to be further worked on). Mouse input. UIanchors fix. Iterators renamed explicitly. Some petty clean up. Performance is probably degraded a tiny bit.

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 function love.load()
+	gameOver = {state = 0, message = "I AM ERROR"}
+	testing = {state = false, tries = 0, moves = 0, matches = 0, dumbness = ""}
 	handsSizes = {0, 0}
 	decksSizes = {0, 0}
 	hands = {{}, {}}
@@ -10,12 +12,13 @@ function love.load()
 	BGgrid = {}
 	recalculateBGgrid = false
 	recalculateUIgrid = false
+	windowMode = {x = 800, y = 480, borderless = true}
 	background = love.graphics.newImage("background.png")
 	cardsAtlas = love.graphics.newImage("cardsAtlas.png")
 	backgroundSpriteBatch = love.graphics.newSpriteBatch(background, 50, "dynamic")
 	cardsSpriteMetaTable = newSpriteMetaTable(cardsAtlas, 59, 81, 117, 0, 0)
 	cardsSpriteBatch = love.graphics.newSpriteBatch(cardsAtlas, 12, "dynamic")
-	love.window.setMode( 800, 480, {borderless = true} )
+	love.window.setMode(windowMode.x, windowMode.y, {borderless = windowMode.borderless})
 	windowWidth, windowHeight = love.graphics.getDimensions()
 	setUIanchors()
 	setBGgrid()
@@ -43,27 +46,63 @@ end
 
 
 function love.update(dt)
-	if love.keyboard.isDown( "z" ) then
+	if love.keyboard.isDown("z") then
 		restart()
-	elseif love.keyboard.isDown( "escape" ) then
+	elseif love.keyboard.isDown("escape") then
 		love.event.quit()
- end
-  for k,v in pairs(keys) do
-    if love.keyboard.isDown(v[1]) then
-      if v[2] then
-        playCard(unpack(v[3]))
-        v[2] = false
-      end
-    else
-      v[2] = true
-    end
-  end
+	elseif love.keyboard.isDown("x") then
+		testing.state = not testing.state
+		windowMode.borderless = not windowMode.borderless
+		love.window.setMode(windowMode.x, windowMode.y, {borderless = windowMode.borderless})
+	end
+	
+	if testing.state then
+		dumbAutoplay()
+	end
+
+	for k, v in pairs(keys) do
+		if love.keyboard.isDown(v[1]) then
+			if v[2] then
+				playCard(unpack(v[3]))
+				v[2] = false
+			end
+		else
+			v[2] = true
+		end
+	end
 end
 
 function love.draw()
 	UIcompose()
 	love.graphics.draw(backgroundSpriteBatch, 0, 0)
 	love.graphics.draw(cardsSpriteBatch, 0, 0)
+	if testing.state then
+		testing.dumbness = string.format("%.f", ((testing.tries - testing.moves) / testing.tries * 100))
+		love.window.setTitle("Tries:" .. testing.tries .. " Moves:" .. testing.moves .. " Matches:" .. testing.matches .. " Dumbness:" .. testing.dumbness .. "%") --Possibly instable.
+	end
+end
+
+function dumbAutoplay()
+	playCard(love.math.random(2), love.math.random(4), love.math.random(2))
+	testing.tries = testing.tries + 1
+end
+
+function isCardHovered(x, y, cardCoordinates)
+	return (x >= cardCoordinates.x and y >= cardCoordinates.y and x <= (cardCoordinates.x + 80) and y <= (cardCoordinates.y + 117))
+end
+
+function love.mousepressed(x, y, button)
+	for player = 1, 2 do
+		for position = 1, 4 do
+			if isCardHovered(x, y, UIanchors.hands[player][position]) then
+				if button == "l" then
+					playCard(player, position, 1)
+				elseif button == "r" then
+					playCard(player, position, 2)
+				end
+			end
+		end
+	end
 end
 
 function setUIanchors()
@@ -72,23 +111,23 @@ function setUIanchors()
 	local xshift = (windowWidth / 6 - 81) / 2
 	local yshift = (windowHeight / 3 - 117) / 2
 	
-	for i = 1, 6 do
-		UIgrid.x[i] = windowWidth / 6 * (i - 1) + xshift
+	for column = 1, 6 do
+		UIgrid.x[column] = windowWidth / 6 * (column - 1) + xshift
 	end
-	for i = 1, 3 do
-		UIgrid.y[i] = windowHeight / 3 * (i - 1) + yshift
+	for column = 1, 3 do
+		UIgrid.y[column] = windowHeight / 3 * (column - 1) + yshift
 	end
 	
 	UIanchors.decks[1] = {x = UIgrid.x[6], y = UIgrid.y[3]}
 	UIanchors.decks[2] = {x = UIgrid.x[1], y = UIgrid.y[1]}
-	UIanchors.hands[1][1] = {x = UIgrid.x[2], y = UIgrid.y[1]}
-	UIanchors.hands[1][2] = {x = UIgrid.x[3], y = UIgrid.y[1]}
-	UIanchors.hands[1][3] = {x = UIgrid.x[4], y = UIgrid.y[1]}
-	UIanchors.hands[1][4] = {x = UIgrid.x[5], y = UIgrid.y[1]}
-	UIanchors.hands[2][1] = {x = UIgrid.x[2], y = UIgrid.y[3]}
-	UIanchors.hands[2][2] = {x = UIgrid.x[3], y = UIgrid.y[3]}
-	UIanchors.hands[2][3] = {x = UIgrid.x[4], y = UIgrid.y[3]}
-	UIanchors.hands[2][4] = {x = UIgrid.x[5], y = UIgrid.y[3]}
+	UIanchors.hands[2][1] = {x = UIgrid.x[2], y = UIgrid.y[1]}
+	UIanchors.hands[2][2] = {x = UIgrid.x[3], y = UIgrid.y[1]}
+	UIanchors.hands[2][3] = {x = UIgrid.x[4], y = UIgrid.y[1]}
+	UIanchors.hands[2][4] = {x = UIgrid.x[5], y = UIgrid.y[1]}
+	UIanchors.hands[1][1] = {x = UIgrid.x[2], y = UIgrid.y[3]}
+	UIanchors.hands[1][2] = {x = UIgrid.x[3], y = UIgrid.y[3]}
+	UIanchors.hands[1][3] = {x = UIgrid.x[4], y = UIgrid.y[3]}
+	UIanchors.hands[1][4] = {x = UIgrid.x[5], y = UIgrid.y[3]}
 	UIanchors.targets[1] = {x = UIgrid.x[3], y = UIgrid.y[2]}
 	UIanchors.targets[2] = {x = UIgrid.x[4], y = UIgrid.y[2]}
 end
@@ -108,7 +147,7 @@ function setBGgrid()
 end
 
 function newSpriteMetaTable(atlas, spriteCount, spriteWidth, spriteHeight, paddingHorizontal, paddingVertical)
-	--This function is meant to be explicitly strict for now
+	--This function is meant to be explicitly strict for now.
 	local x_offset = 0
 	local y_offset = 0
 	local cardsSpriteMetaTable = {}
@@ -121,9 +160,9 @@ function newSpriteMetaTable(atlas, spriteCount, spriteWidth, spriteHeight, paddi
 		love.window.showMessageBox("Error", "Atlas is corrupted: size multiplicity mismatch", "error", true)
 		love.event.quit()
 	else
-		for i = 0, spriteCount do
-			x_offset = (i % spriteCountHorizontal) * (spriteWidth + paddingHorizontal)
-			y_offset = ({math.modf(i / spriteCountHorizontal)})[1] * (spriteHeight + paddingVertical)
+		for sprite = 0, spriteCount do
+			x_offset = (sprite % spriteCountHorizontal) * (spriteWidth + paddingHorizontal)
+			y_offset = ({math.modf(sprite / spriteCountHorizontal)})[1] * (spriteHeight + paddingVertical)
 			quad = love.graphics.newQuad(x_offset, y_offset, spriteWidth, spriteHeight, ({atlas:getDimensions()})[1], ({atlas:getDimensions()})[2])
 			spriteMeta = {quad, 0, 0}
 			table.insert(cardsSpriteMetaTable, spriteMeta)
@@ -139,31 +178,31 @@ function deal()
 	local randomIndex = nil
 
 	--Populate
-	for i = 1, 52 do
-		deck1[i] = i
+	for card = 1, 52 do
+		deck1[card] = card
 	end
 
 	--Shuffle
-	for i = 1, 52 do
-		randomIndex = love.math.random(i, 52)
-		j = deck1[i]
-		deck1[i] = deck1[randomIndex]
+	for card = 1, 52 do
+		randomIndex = love.math.random(card, 52)
+		j = deck1[card]
+		deck1[card] = deck1[randomIndex]
 		deck1[randomIndex] = j
 	end
 
 	--Split
-	for i = 1, 26 do
-		deck2[i] = deck1[i]
-		deck1[i] = deck1[i + 26]
-		deck1[i + 26] = nil
+	for card = 1, 26 do
+		deck2[card] = deck1[card]
+		deck1[card] = deck1[card + 26]
+		deck1[card + 26] = nil
 	end
 
 	decks = {deck1, deck2}
 	decksSizes = {26, 26}
 	
-	for i = 1, 2 do
-		for j = 1, 4 do
-			drawCard(i, j)
+	for player = 1, 2 do
+		for position = 1, 4 do
+			drawCard(player, position)
 		end
 	end
 	
@@ -186,10 +225,15 @@ end
 
 function playCard(player, position, target)
 	if isValid(player, position, target) then
+		
+		if testing.state then
+			testing.moves = testing.moves + 1
+		end
+			
 		targets[target] = hands[player][position]
 		handsSizes[player] = handsSizes[player] - 1
 		drawCard(player, position)
-		isGameOver()
+		checkGameOver()
 		
 		if isStalemate() then
 			resolve()
@@ -197,46 +241,40 @@ function playCard(player, position, target)
 	end
 end
 
-function isGameOver()
-	local condition = false --false — none, 1 — first, 2 — second, 3 — both players win(draw).
-	
+function checkGameOver() --0 — none, 1 — first, 2 — second, 3 — both players win(draw).
 	if handsSizes[1] == 0 and handsSizes[2] == 0 then
-		condition = 3
+		gameOver.state = 3
+		gameOver.message = "It's a draw!"
 	elseif handsSizes[1] == 0 then
-		condition = 1
+		gameOver.state = 1
+		gameOver.message = "First player won!"
 	elseif handsSizes[2] == 0 then
-		condition = 2
+		gameOver.state = 2
+		gameOver.message = "Second player won!"
 	end
-	
-	if condition ~= false then --Placeholder for a return.
-		gameOver(condition)
+
+	if gameOver.state ~= 0 then --This need to be elsewhere but major issues were encountered.
+		endGame()
 	end
 end
 
-function gameOver(condition)
-	local message
-
-	if condition == false then --Need to put a switch instead.
-		message = "I AM ERROR"
-	elseif condition == 1 then
-		message = "First player won!"
-	elseif condition == 2 then
-		message = "Second player won!"
-	elseif condition == 3 then
-		message = "It's a draw!"
+function endGame()
+	if testing.state then
+		testing.matches = testing.matches + 1
+	else
+		love.window.showMessageBox("Game Over", gameOver.message, "info", true) --Needs to visually resolve first.
 	end
 
-	love.window.showMessageBox("Game Over", message, "info", true) --Needs to visually resolve first.
 	restart()
 end
 
 function isStalemate()
 	local invalidity = true
 
-	for i = 1, 2 do
-		for j = 1, 4 do
-			for k = 1, 2 do
-				if isValid(i, j, k) then
+	for player = 1, 2 do
+		for position = 1, 4 do
+			for target = 1, 2 do
+				if isValid(player, position, target) then
 					invalidity = false
 				end
 			end
@@ -258,19 +296,19 @@ end
 function resolve()
 	local candidates = {}
 
-	for i = 1, 2 do
-		if decksSizes[i] == 0 then
-			for j = 1, 4 do
-				if hands[i][j] ~= 59 then
-					candidates[i] = hands[i][j]
-					hands[i][j] = 59
-					handsSizes[i] = handsSizes[i] - 1
-					isGameOver()
-					break --Might be causing an unknown bug.
+	for player = 1, 2 do
+		if decksSizes[player] == 0 then
+			for position = 1, 4 do
+				if hands[player][position] ~= 59 then
+					candidates[player] = hands[player][position]
+					hands[player][position] = 59
+					handsSizes[player] = handsSizes[player] - 1
+					checkGameOver()
+					break
 				end
 			end
 		else
-		candidates[i] = popDeck(i)
+		candidates[player] = popDeck(player)
 		end
 	end
 
@@ -284,11 +322,12 @@ end
 
 function isValid(player, position, target)
 	local validity = nil
-
+	local difference = nil
+	
 	if hands[player][position] == 59 then
 		validity = false
 	else
-		local difference = math.abs(hands[player][position] % 13 - targets[target] % 13)
+		difference = math.abs(hands[player][position] % 13 - targets[target] % 13)
 		validity = difference == 1 or difference == 12
 	end
 	
@@ -296,6 +335,7 @@ function isValid(player, position, target)
 end
 
 function restart()
+	gameOver = {state = 0, message = "I AM ERROR"}
 	deal()
 end
 
@@ -304,8 +344,8 @@ function BGcompose()
 		setBGgrid()
 	end
 	
-	for i = 1, #BGgrid do
-	backgroundSpriteBatch:add(unpack(BGgrid[i]))
+	for tile = 1, #BGgrid do
+	backgroundSpriteBatch:add(unpack(BGgrid[tile]))
 	end
 end
 
@@ -319,31 +359,31 @@ function UIcompose()
 	cardsSpriteBatch:clear()
 
 	--Decks
-	for i = 1, 2 do
-		if decksSizes[i] > 0 then
+	for deck = 1, 2 do
+		if decksSizes[deck] > 0 then
 			sprite = 53
 		else
 			sprite = 59
 		end
 
-		cardsSpriteMetaTable[sprite][2] = UIanchors.decks[i].x
-		cardsSpriteMetaTable[sprite][3] = UIanchors.decks[i].y
+		cardsSpriteMetaTable[sprite][2] = UIanchors.decks[deck].x
+		cardsSpriteMetaTable[sprite][3] = UIanchors.decks[deck].y
 		cardsSpriteBatch:add(unpack(cardsSpriteMetaTable[sprite]))
 	end
 
 	--Hands
-	for i = 1, 2 do
-		for j = 1, 4 do
-			cardsSpriteMetaTable[hands[i][j]][2] = UIanchors.hands[i][j].x
-			cardsSpriteMetaTable[hands[i][j]][3] = UIanchors.hands[i][j].y
-			cardsSpriteBatch:add(unpack(cardsSpriteMetaTable[hands[i][j]]))
+	for player = 1, 2 do
+		for position = 1, 4 do
+			cardsSpriteMetaTable[hands[player][position]][2] = UIanchors.hands[player][position].x
+			cardsSpriteMetaTable[hands[player][position]][3] = UIanchors.hands[player][position].y
+			cardsSpriteBatch:add(unpack(cardsSpriteMetaTable[hands[player][position]]))
 		end
 	end
 	
 	--Targets
-	for i = 1, 2 do
-		cardsSpriteMetaTable[targets[i]][2] = UIanchors.targets[i].x
-		cardsSpriteMetaTable[targets[i]][3] = UIanchors.targets[i].y
-		cardsSpriteBatch:add(unpack(cardsSpriteMetaTable[targets[i]]))
+	for target = 1, 2 do
+		cardsSpriteMetaTable[targets[target]][2] = UIanchors.targets[target].x
+		cardsSpriteMetaTable[targets[target]][3] = UIanchors.targets[target].y
+		cardsSpriteBatch:add(unpack(cardsSpriteMetaTable[targets[target]]))
 	end
 end
